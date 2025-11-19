12dc79f68eb9ebf622312b18f3568dd4b446a76f
Анатолий Цыбульский <tolekmm@Anatolijs-Mac-mini.local>
2025-11-19 23:34:52 +0300

    Скопировал проект Шумейко

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"msg": "Hello World!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
